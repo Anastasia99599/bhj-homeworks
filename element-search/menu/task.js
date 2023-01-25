99d21546152a09ba13d708743643b69d2dad2e24
Анастасия Дунаева <vselennaia.beskonecha@yandex.ru>
2023-01-25 22:01:07 +0300

    first commit

---
+++ element-search/menu/task.js
@@ -0,0 +1,20 @@
+const links = [...document.querySelectorAll('.menu__link')];
+const subMenu = [...document.querySelectorAll('.menu_sub')];
+
+function closeSub (current = null) {
+    subMenu.forEach(item => {
+        if (item != current) item.classList.remove('menu_active');
+    });
+}
+
+links.forEach(link => {
+    link.addEventListener('click', (event) => {
+        console.log(event);
+        if (link.nextElementSibling) {
+            event.preventDefault();
+            closeSub(event.target.nextElementSibling);
+            event.target.nextElementSibling.classList.toggle('menu_active');
+            
+        }
+    });
+})
